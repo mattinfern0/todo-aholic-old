0c59baa73444bf6bf98d1420a7be7745705e12bd
Matthew Fernandez <mattfer98@gmail.com>
2019-09-07 20:09:09 -0700

    Started server code

---
+++ server/routes/api.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+
+/* Routes:
+    Create task: POST /tasks/create
+    Get task: GET /tasks/:taskId
+    Update task: PUT /tasks/:taskId
+    Delete task: DELETE /tasks/:taskId
+
+    Create project: POST /projects/create
+    Get project: GET //projects/:projectId
+    Update project: PUT /projects/:projectId
+    Delete project: DELETE /projects/:projectId
+*/
