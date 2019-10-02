719e9050453fec9b999af6d41fa77cfe77f8b7c2
Matthew Fernandez <mattfer98@gmail.com>
2019-10-01 20:24:22 -0700

    PUT & DELETE for users

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,6 +1,7 @@
 const jwt = require('jsonwebtoken');
 const passport = require('passport');
 const bcrypt = require('bcryptjs');
+const mongoose = require('mongoose');
 
 const async = require('async');
 const { body, validationResult, sanitizeBody } = require('express-validator');
@@ -22,11 +23,10 @@ exports.loginUser = (req, res, next) => {
         next(err);
       }
 
-      // Send user's id only for security purposes
-      const idOnly = { _id: user._id };
+      const noPassword = { _id: user._id, username: user.username };
 
       const token = jwt.sign(user.toJSON(), process.env.JWT_SECRET, { expiresIn: '3 days' });
-      return res.json({ user: idOnly, token });
+      return res.json({ user: noPassword, token });
     });
   })(req, res, next);
 };
@@ -75,9 +75,79 @@ exports.createUser = [
             next(err);
           }
 
-          return res.status(201).json({ newUser });
+          const noPassword = {
+            _id: newUser._id,
+            username: newUser.username,
+          };
+
+          return res.status(201).json({ newUser: noPassword });
         });
       });
     });
   },
 ];
+
+exports.updateUser = [
+  body('newPassword', 'New password must be at least 8 characters').isLength({ min: 8 }).trim(),
+  sanitizeBody('*').escape(),
+  (req, res, next) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json(errors);
+    }
+
+    const userId = req.params.userId;
+    if (!mongoose.Types.ObjectId.isValid(userId)) {
+      return res.status(400).json({ message: 'Invalid user ID' });
+    }
+
+    User.findById(userId, (err, theUser) => {
+      if (err) {
+        return next(err);
+      }
+
+      bcrypt.compare(req.body.oldPassword, theUser.password, (err, result) => {
+        if (err) {
+          return next(err);
+        } else if (!result) {
+          return res.status(400).json({ message: 'Old password is incorrect' });
+        }
+
+        bcrypt.hash(req.body.newPassword, 10, (err, hashedPassword) => {
+          if (err) {
+            return next(err);
+          }
+
+          const updatedInfo = {
+            password: hashedPassword,
+          };
+
+          User.findByIdAndUpdate(userId, updatedInfo, (err, updatedUser) => {
+            if (err) {
+              return next(err);
+            }
+
+            const updatedToken = jwt.sign(updatedUser.toJSON(), process.env.JWT_SECRET, { expiresIn: '3 days' });
+            return res.status(200).json({ token: updatedToken });
+          });
+        });
+      });
+    });
+  },
+];
+
+exports.deleteUser = (req, res, next) => {
+  const userId = req.params.userId;
+  if (!mongoose.Types.ObjectId.isValid(userId)) {
+    return res.status(400).json({ message: 'Invalid user ID' });
+  }
+
+  User.findByIdAndDelete(userId, (err) => {
+    if (err) {
+      return next(err);
+    }
+
+    return res.sendStatus(200);
+  });
+};
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const passport = require('passport');
 const userController = require('../controllers/userController');
 
 const router = express.Router();
@@ -6,5 +7,7 @@ const router = express.Router();
 router.post('/checkToken'); // Will be used to check for expired tokens
 router.post('/', userController.createUser);
 router.post('/login', userController.loginUser);
+router.put('/:userId', passport.authenticate('jwt', { session: false }), userController.updateUser);
+router.delete('/:userId', passport.authenticate('jwt', { session: false }), userController.deleteUser);
 
 module.exports = router;
